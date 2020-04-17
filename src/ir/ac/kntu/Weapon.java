bb8476ecb1ea5b4e720043080b142f6ae99cf993
Ghazal Pouresfandiyar <ghazal.brn@gmail.com>
2020-04-17 16:56:56 +0430

    dalan_marg

---
+++ src/ir/ac/kntu/Weapon.java
@@ -0,0 +1,8 @@
+package ir.ac.kntu;
+
+public class Weapon {
+    private String name;
+    private double faultRate;
+    private double accuracyRate; //1-faultRate;
+    private double damageRate;
+}
